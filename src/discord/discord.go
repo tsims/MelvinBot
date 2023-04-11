2f4a200ca56648b37bc94e05a1b99c40778af1df
ndevasia <ndevasia@mit.edu>
2023-04-10 17:24:53 -0700

    bot commands for nisha's discord

--- src/discord/discord.go
+++ src/discord/discord.go
@@ -56,6 +56,9 @@ func (bot Bot) RunBot() {
 	bot.discord.AddHandler(stats.PrintStats)
 	bot.discord.AddHandler(pinFromReaction)
 	bot.discord.AddHandler(unpinFromReaction)
+	bot.discord.AddHandler(didSomebodySaySex)
+	bot.discord.AddHandler(thisIsNotADvd)
+	bot.discord.AddHandler(georgeCarlin)
 
 	err = bot.discord.Open()
 	if err != nil {
@@ -85,4 +88,51 @@ func monkaS(s *disc.Session, m *disc.MessageCreate) {
 	if strings.Contains(strings.ToLower(m.Message.Content), "monkas") {
 		s.ChannelMessageSend(m.ChannelID, "monkaS")
 	}
+}
+
+func didSomebodySaySex(s *disc.Session, m *disc.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return // it me
+	}
+
+	if m.GuildID != util.wolfcord_id {
+		return // only for nisha's discord
+	}
+
+	if strings.Contains(strings.ToLower(m.Message.Content), "sex") {
+		s.ChannelMessageSend(m.ChannelID, "did somebody say sex???")
+	}
+}
+
+func thisIsNotADvd(s *disc.Session, m *disc.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return // it me
+	}
+
+	if m.GuildID != util.wolfcord_id {
+		return // only for nisha's discord
+	}
+
+	if m.Content != "!stop" {
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "STOP! STOP! STOP! This is NOT a DVD. This is NOT A DVD. THIS IS NOT A DVD. This is a BACKER CARD. It's a CARD for COLLECTORS. This is a MOVIE CARD. THIS IS NOT A DVD. STOP! READ. READ THE DESCRIPTION.")
+
+}
+
+func georgeCarlin(s *disc.Session, m *disc.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return // it me
+	}
+
+	if m.GuildID != util.wolfcord_id {
+		return // only for nisha's discord
+	}
+
+	if m.Content != "!rsbs" {
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "RATSHIT BATSHIT DIRTY OLD TWAT 69 ASSHOLES TIED IN A KNOT HOORAY LIZARD SHIT FUCK")
 }
